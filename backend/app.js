c3d8deec017306adc1f05aed72ab1038dc5d6908
Abhishek Silawat <absilawat@gmail.com>
2022-09-27 21:14:03 +0530

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const authRoute = require("./src/routes/authRoute");
+const hotelsRoute = require("./src/routes/hotelsRoute");
+const roomsRoute = require("./src/routes/roomsRoute");
+const usersRoute = require("./src/routes/usersRoute");
+
+const app = express();
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+module.exports = app;

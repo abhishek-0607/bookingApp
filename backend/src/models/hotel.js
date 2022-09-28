70db6b98f5addfe6c8a1d7dd6d60c0fe784f2618
Abhishek Silawat <absilawat@gmail.com>
2022-09-28 09:56:11 +0530

    Create hotel.js

---
+++ backend/src/models/hotel.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require("mongoose");
+
+const hotelSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    distance: {
+      type: String,
+      required: true,
+    },
+    photos: {
+      type: [String],
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    rooms: {
+      type: [String],
+    },
+    cheapestPrice: {
+      type: Number,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Hotel = model("hotel", hotelSchema);
+module.exports = Hotel;

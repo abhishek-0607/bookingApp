30153f0098d4f013de2f87cb52e9e0fee8cc9fd7
Abhishek Silawat <absilawat@gmail.com>
2022-09-30 01:09:10 +0530

    Update authController.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,12 +1,46 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const createError = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res, next) => {
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(req.body.password, salt);
   try {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
     return res.status(200).send("User has been registered.");
   } catch (e) {
     next(e);
   }
 };
 
-module.exports = { register };
+const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return next(createError(404, "username not found!!!"));
+    }
+
+    const isPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!isPassword) {
+      return next(createError(400, "wrong username or password!"));
+    }
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.KEY
+    );
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    return res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ ...otherDetails });
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = { register, login };

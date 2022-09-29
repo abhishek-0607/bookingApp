6d749160166c07bfce18c8db3a4865df27ce8594
Abhishek Silawat <absilawat@gmail.com>
2022-09-30 01:09:41 +0530

    Update usersRoute.js

--- backend/src/routes/usersRoute.js
+++ backend/src/routes/usersRoute.js
@@ -1,4 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const {
+  createUser,
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} = require("../controllers/UsersController");
+const verifyToken = require("../utils/verifyToken");
+
+router.get("/check", verifyToken, (req, res, next) => {
+  res.send("hello user,you are logged in");
+});
+
+//CREATE
+router.post("/", createUser);
+
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/", getUsers);
 
 module.exports = router;

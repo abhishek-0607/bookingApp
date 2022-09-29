8a552d6ce02f12d98f1436321ec38afcbc05b6e9
Abhishek Silawat <absilawat@gmail.com>
2022-09-29 15:13:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,30 @@ const roomsRoute = require("./src/routes/roomsRoute");
 const usersRoute = require("./src/routes/usersRoute");
 
 const app = express();
+
+//middlewares
+// app.use((req, res, next) => {
+//   console.log("I'm middleware");
+//   next();
+// });
+
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
+// error handling middleware
+// app.use((err, req, res, next) => {
+//   const errStatus = err.status || 500;
+//   const errMessage = err.message || "something went wrong!!!";
+//   return res.status(errStatus).json({
+//     success: false,
+//     status: errStatus,
+//     message: errMessage,
+//     stack: err.stack,
+//   });
+// });
 
 module.exports = app;

8d1205a434895c8e3a308ae264dc38aa4f7878e9
Abhishek Silawat <absilawat@gmail.com>
2022-10-01 22:40:21 +0530

    Update roomsRoute.js

--- backend/src/routes/roomsRoute.js
+++ backend/src/routes/roomsRoute.js
@@ -1,4 +1,24 @@
 const express = require("express");
+const {
+  createRoom,
+  deleteRoom,
+  updateRoom,
+  getRoom,
+  getRooms,
+} = require("../controllers/roomsControllers");
 const router = express.Router();
+
+const { verifyAdmin } = require("../utils/verifyToken");
+
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteRoom);
+//GET
+router.get("/:id", getRoom);
+//GET ALL
+router.get("/", getRooms);
 
 module.exports = router;

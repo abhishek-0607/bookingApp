76afbe16d2536597b3f8fc54ffd9b10de2b25926
Abhishek Silawat <absilawat@gmail.com>
2022-10-01 22:40:30 +0530

    Update verifyToken.js

--- backend/src/utils/verifyToken.js
+++ backend/src/utils/verifyToken.js
@@ -14,4 +14,28 @@ const verifyToken = (req, res, next) => {
     next();
   });
 };
-module.exports = verifyToken;
+
+const verifyUser = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      if (err) {
+        return next(createError(403, "you are not authorized!"));
+      }
+    }
+  });
+};
+
+const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      if (err) {
+        return next(createError(403, "you are not authorized!"));
+      }
+    }
+  });
+};
+module.exports = { verifyToken, verifyUser, verifyAdmin };

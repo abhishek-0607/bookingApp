19b995f44f79852064ea419619524aa0c83b4d70
Abhishek Silawat <absilawat@gmail.com>
2022-10-03 02:02:47 +0530

    Update roomsRoute.js

--- backend/src/routes/roomsRoute.js
+++ backend/src/routes/roomsRoute.js
@@ -15,7 +15,7 @@ router.post("/:hotelid", verifyAdmin, createRoom);
 //UPDATE
 router.put("/:id", verifyAdmin, updateRoom);
 //DELETE
-router.delete("/:id", verifyAdmin, deleteRoom);
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
 //GET
 router.get("/:id", getRoom);
 //GET ALL

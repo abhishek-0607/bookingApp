91dc088fcec456c345ffd8cd4a00ffd5283d3df7
Abhishek Silawat <absilawat@gmail.com>
2022-10-05 04:27:34 +0530

    created home

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../../components/header/Header";
+import { Navbar } from "../../components/Navbar/Navbar";
+import "./home.css";
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+};

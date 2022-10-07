c91f42ea0fb22ca06d17a1c461d6273b7a7baf61
Abhishek Silawat <absilawat@gmail.com>
2022-10-07 18:35:13 +0530

    updated home by adding components

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { Header } from "../../components/header/Header";
-import { Navbar } from "../../components/Navbar/Navbar";
+import { Navbar } from "../../components/navbar/Navbar";
+import { Featured } from "../../components/featured/Featured";
+import { PropertyList } from "../../components/propertyList/PropertyList";
+
 import "./home.css";
+import { FeaturedProperties } from "../../components/featuredProperties/FeaturedProperties";
 export const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="home-container">
+        <Featured />
+        <h1 className="home-title">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="home-title">Home guests love</h1>
+        <FeaturedProperties />
+      </div>
     </div>
   );
 };

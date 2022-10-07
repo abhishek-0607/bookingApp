10b68be028cc9f02dab7830498082b4a6a44960c
Abhishek Silawat <absilawat@gmail.com>
2022-10-08 02:40:39 +0530

    updated App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Routes,
-  Link,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/home/Home";
 import { Hotel } from "./pages/hotel/Hotel";

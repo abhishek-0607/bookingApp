98497d92df8c0e77c1ef3d4c702a80eabd1f480e
Abhishek Silawat <absilawat@gmail.com>
2022-10-08 02:40:56 +0530

    Update FeaturedProperties.jsx

--- frontend/src/components/featuredProperties/FeaturedProperties.jsx
+++ frontend/src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,5 +2,60 @@ import React from "react";
 import "./featuredProperties.css";
 
 export const FeaturedProperties = () => {
-  return <div className="featured-properties"></div>;
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+        />
+        <span className="fpName">Sayaji Hotel</span>
+        <span className="fpCity">Bhopal</span>
+        <span className="fpPrice">starting from Rs. 5000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+        />
+        <span className="fpName">Sayaji Hotel</span>
+        <span className="fpCity">Bhopal</span>
+        <span className="fpPrice">starting from Rs. 5000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+        />
+        <span className="fpName">Sayaji Hotel</span>
+        <span className="fpCity">Bhopal</span>
+        <span className="fpPrice">starting from Rs. 5000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+        />
+        <span className="fpName">Sayaji Hotel</span>
+        <span className="fpCity">Bhopal</span>
+        <span className="fpPrice">starting from Rs. 5000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
 };

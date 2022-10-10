855bc324738597dfbbff014983e1142aea0817e4
Abhishek Silawat <absilawat@gmail.com>
2022-10-10 22:57:44 +0530

    Update hotelsController.js

--- backend/src/controllers/hotelsController.js
+++ backend/src/controllers/hotelsController.js
@@ -52,4 +52,46 @@ const getHotels = async (req, res, next) => {
     next(e);
   }
 };
-module.exports = { createHotel, updateHotel, deleteHotel, getHotel, getHotels };
+const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
+  } catch (e) {
+    //   res.status(500).json({ status: "failed", message: e.message });
+    next(e);
+  }
+};
+const countByType = async (req, res, next) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+
+    res.status(200).json([
+      { type: "hotel", count: hotelCount },
+      { type: "apartment", count: apartmentCount },
+      { type: "resort", count: resortCount },
+      { type: "villa", count: villaCount },
+      { type: "cabin", count: cabinCount },
+    ]);
+  } catch (e) {
+    //   res.status(500).json({ status: "failed", message: e.message });
+    next(e);
+  }
+};
+module.exports = {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  countByCity,
+  countByType,
+};

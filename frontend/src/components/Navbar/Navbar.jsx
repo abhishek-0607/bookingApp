57f5404440e53ff27205fe67bec77aa14c436c8c
Abhishek Silawat <absilawat@gmail.com>
2022-10-11 21:12:38 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="nav-container">
-        <span className="logo">Booking.com</span>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">Booking.com</span>
+        </Link>
         <div className="nav-item">
           <button className="nav-button">Register</button>
           <button className="nav-button">Login</button>

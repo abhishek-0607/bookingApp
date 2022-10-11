e5a335ffd3554748610b7b70c2a29067cf7aa95c
Abhishek Silawat <absilawat@gmail.com>
2022-10-11 21:12:29 +0530

    Update FeaturedProperties.jsx

--- frontend/src/components/featuredProperties/FeaturedProperties.jsx
+++ frontend/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,61 +1,34 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
 
 export const FeaturedProperties = () => {
+  const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
+  console.log(data);
   return (
     <div className="fp">
-      <div className="fpItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-        />
-        <span className="fpName">Sayaji Hotel</span>
-        <span className="fpCity">Bhopal</span>
-        <span className="fpPrice">starting from Rs. 5000</span>
-        <div className="fpRating">
-          <button>8.9</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-      <div className="fpItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-        />
-        <span className="fpName">Sayaji Hotel</span>
-        <span className="fpCity">Bhopal</span>
-        <span className="fpPrice">starting from Rs. 5000</span>
-        <div className="fpRating">
-          <button>8.9</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-      <div className="fpItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-        />
-        <span className="fpName">Sayaji Hotel</span>
-        <span className="fpCity">Bhopal</span>
-        <span className="fpPrice">starting from Rs. 5000</span>
-        <div className="fpRating">
-          <button>8.9</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-      <div className="fpItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
-          alt=""
-        />
-        <span className="fpName">Sayaji Hotel</span>
-        <span className="fpCity">Bhopal</span>
-        <span className="fpPrice">starting from Rs. 5000</span>
-        <div className="fpRating">
-          <button>8.9</button>
-          <span>Excellent</span>
-        </div>
-      </div>
+      {loading ? (
+        "Loading"
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              <img src={item.photos[0]} alt="" />
+              <span className="fpName">{item.name}</span>
+              <span className="fpCity">{item.city}</span>
+              <span className="fpPrice">
+                starting from Rs.{item.cheapestPrice}
+              </span>
+              {item.rating && (
+                <div className="fpRating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };

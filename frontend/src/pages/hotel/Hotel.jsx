0062cac133e78133b99771347ced2843bf3871cb
Abhishek Silawat <absilawat@gmail.com>
2022-10-12 14:05:11 +0530

    Update Hotel.jsx

--- frontend/src/pages/hotel/Hotel.jsx
+++ frontend/src/pages/hotel/Hotel.jsx
@@ -6,20 +6,19 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Footer } from "../../components/footer/Footer";
 import { Header } from "../../components/header/Header";
 import { MailList } from "../../components/mailList/MailList";
 import { Navbar } from "../../components/navbar/Navbar";
+import { SearchContext } from "../../context/searchContext";
 import useFetch from "../../hooks/useFetch";
 import "./hotel.css";
 export const Hotel = () => {
   const location = useLocation();
   const id = location.pathname.split("/")[2];
   console.log(id);
-  const { data, loading, error } = useFetch(`/hotels/find/${id}`);
-  console.log(data);
 
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
@@ -27,6 +26,18 @@ export const Hotel = () => {
     setSlideNumber(i);
     setOpen(true);
   };
+  const { data, loading, error } = useFetch(`/hotels/find/${id}`);
+  console.log(data);
+
+  const { dates, options } = useContext(SearchContext);
+  console.log(dates);
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+    return diffDays;
+  }
+  const days = dayDifference(dates[0].endDate, dates[0].startDate);
 
   const handleMove = (direction) => {
     let newSlideNumber;
@@ -109,13 +120,14 @@ export const Hotel = () => {
                 <p className="hotelDesc">{data.desc}</p>
               </div>
               <div className="hotelDetailsPrice">
-                <h1>Perfect for a 9-night stay!</h1>
+                <h1>Perfect for a {days}-night stay!</h1>
                 <span>
                   Located in the real heart of Krakow, this property has an
                   excellent location score of 9.8!
                 </span>
                 <h2>
-                  <b>Rs.{data.cheapestPrice}</b> (9 nights)
+                  <b>Rs.{days * data.cheapestPrice * options.room}</b> ({days}
+                  nights)
                 </h2>
                 <button>Reserve or Book Now!</button>
               </div>

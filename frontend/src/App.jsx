d6c817d02b83a02501d996a8c5bbce2c1b83f29a
Abhishek Silawat <absilawat@gmail.com>
2022-10-13 20:27:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Home } from "./pages/home/Home";
 import { Hotel } from "./pages/hotel/Hotel";
 import { List } from "./pages/list/List";
+import { Login } from "./pages/login/Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/hotels" element={<List />} />
         <Route exact path="/hotels/:id" element={<Hotel />} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );

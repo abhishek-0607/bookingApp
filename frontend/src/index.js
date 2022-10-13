a33a65384e509dda1510f58dd32bbab2cf389c74
Abhishek Silawat <absilawat@gmail.com>
2022-10-13 20:28:04 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { SearchContextProvider } from "./context/searchContext";
+import { AuthContextProvider } from "./context/authContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SearchContextProvider>
-      <App />
-    </SearchContextProvider>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 

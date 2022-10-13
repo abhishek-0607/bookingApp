eee5d97d6c4449fa911711c08b24f82d7cbc02d1
Abhishek Silawat <absilawat@gmail.com>
2022-10-13 20:27:56 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
 import "./navbar.css";
 
 export const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="nav-container">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">Booking.com</span>
         </Link>
-        <div className="nav-item">
-          <button className="nav-button">Register</button>
-          <button className="nav-button">Login</button>
-        </div>
+        {user ? (
+          user.username
+        ) : (
+          <div className="nav-item">
+            <button className="nav-button">Register</button>
+            <button className="nav-button">Login</button>
+          </div>
+        )}
       </div>
     </div>
   );
